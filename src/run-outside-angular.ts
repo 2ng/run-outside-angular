563be344fb9083a1bce85dfd15fadeccc347d66e
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-01-30 22:52:36 +0300

    feat: add support setters methods

--- src/run-outside-angular.ts
+++ src/run-outside-angular.ts
@@ -21,17 +21,31 @@ function throwError(targetName) {
 export function RunOutsideAngular(config?: Config): ClassDecorator {
   return function(constructor: any) {
     const excludedMethods = config ? [...DEFAULT_EXCLUDED_METHODS, ...config.exclude] : DEFAULT_EXCLUDED_METHODS;
+
     const methods = Object.getOwnPropertyNames(constructor.prototype);
 
-    methods.forEach(method => {
-      if (!excludedMethods.includes(method)) {
-        const original = constructor.prototype[method];
+    methods
+      .filter(method => !excludedMethods.includes(method))
+      .forEach(method => {
+        if (Object.getOwnPropertyDescriptor(constructor.prototype, method).set) {
+          const original = Object.getOwnPropertyDescriptor(constructor.prototype, method).set;
 
-        constructor.prototype[method] = function(...args: string[]) {
-          if (!this._ngZone) throwError(constructor.name);
-          return this._ngZone.runOutsideAngular(() => original.apply(this, args));
-        };
-      }
-    });
+          Object.defineProperty(constructor.prototype, method, {
+            set: function(...args) {
+              if (!this._ngZone) throwError(constructor.name);
+              this._ngZone.runOutsideAngular(() => original.apply(this, args));
+            },
+          });
+        }
+
+        if (typeof constructor.prototype[method] === 'function') {
+          const original = constructor.prototype[method];
+
+          constructor.prototype[method] = function(...args: string[]) {
+            if (!this._ngZone) throwError(constructor.name);
+            return this._ngZone.runOutsideAngular(() => original.apply(this, args));
+          };
+        }
+      });
   };
 }
